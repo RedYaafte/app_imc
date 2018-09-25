d54acbf910e83ee0371c510be8073b7748a96f2e
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-09-24 20:46:38 -0500

    HotFix: Commented all example code.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 
 // Seleccion del DOM
-var h1 = document.querySelector('h1');
-console.log(h1);
+// var h1 = document.querySelector('h1');
+// console.log(h1);
 
-var input = document.querySelector('#dataInput');
-input.value = 'Hola ¿Cómo estás?';
-console.log(input.value);
-console.log(input);
+// var input = document.querySelector('#dataInput');
+// input.value = 'Hola ¿Cómo estás?';
+// console.log(input.value);
+// console.log(input);
 
-var items = document.querySelectorAll('li');
-console.log(items);
+// var items = document.querySelectorAll('li');
+// console.log(items);
 
 // Modificar el DOM
 
-h1.textContent = "Foooo<p>Hola</p>"; // Modifica solo el nodo de texto
-h1.innerHTML = "Foooo<p>Hola</p>"; // Puede agregar HTML
+// h1.textContent = "Foooo<p>Hola</p>"; // Modifica solo el nodo de texto
+// h1.innerHTML = "Foooo<p>Hola</p>"; // Puede agregar HTML
 
 // Event Listeners
 
-var button = document.querySelector('button');
+// var button = document.querySelector('button');
 
-button.addEventListener('click', (evt) => {
-    console.log(evt.target);
-    console.log('Ouch!!!');
-})
+// button.addEventListener('click', (evt) => {
+//     console.log(evt.target);
+//     console.log('Ouch!!!');
+// })
+
+
+// Ejercicio.
+// Registrar peso y altura a travez de inputs.
+
+// Un boton que al dar click tome valor de peso y altura
+// y calcule imc.
+
+// lo debe de guardar en un arreglo y mostrarlo al final del documento.
