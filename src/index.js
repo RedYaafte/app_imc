a9ab8db6c74563ebf23f40c595a2cbdd3b023824
RedYaafte <redyafte@outlook.com>
2018-09-26 19:07:27 -0500

    segund push

--- src/index.js
+++ src/index.js
@@ -33,3 +33,62 @@
 // y calcule imc.
 
 // lo debe de guardar en un arreglo y mostrarlo al final del documento.
+
+// id
+var inputPeso = document.querySelector('#dataPeso');
+var inputAltura = document.querySelector('#dataAltura');
+var resultado = document.querySelector('#resultado');
+// Fecha actual
+var d = new Date();
+var date = d.toDateString();
+// button
+var button = document.querySelector('button');
+
+
+button.addEventListener('click', () => {
+    var intPeso = inputPeso.value;
+    var intAltura = inputAltura.value;
+    var result2 = intPeso / intAltura **2;
+    var result = result2.toFixed(2);
+
+    var functionArray = new function () {
+      return {
+        peso: intPeso,
+        altura: intAltura,
+        imc: result,
+        fecha: date
+      }
+    }
+
+    // sessionStorage.setItem('datos', JSON.stringify(functionArray))
+    // var guardados = [sessionStorage.getItem('datos')]
+
+    var guardados = [functionArray]
+
+    // console.log(guardados);
+    resultado.innerHTML = JSON.stringify(guardados)
+    console.log(guardados);
+
+    // Canvas
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: ['martes', 'miercoles', 'jueves'],
+        datasets: [{
+          label: "Tu Indice de Masa Corporal",
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [1, 5, 10],
+        }]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+
+
+})
